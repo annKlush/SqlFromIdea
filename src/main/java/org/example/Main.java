47629c267aae3f0ce4f88ed07adefac0a97664da
annKlush <109134951+annKlush@users.noreply.github.com>
2023-02-01 12:07:23 +0200

    up

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,11 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Database date = Database.getInstance();
         date.getConnection();
-        System.out.println(new DatabaseQueryService().findMaxSalary().toString());
-        System.out.println(new DatabaseQueryService().findMaxProjectCountClient().toString());
-        System.out.println(new DatabaseQueryService().findLongestPrs().toString());
-        System.out.println(new DatabaseQueryService().findYoungOldWorker().toString());
-        System.out.println(new DatabaseQueryService().printPrPrice().toString());
+        System.out.println(new DatabaseQueryService(date).findMaxSalary().toString());
+        System.out.println(new DatabaseQueryService(date).findMaxProjectCountClient().toString());
+        System.out.println(new DatabaseQueryService(date).findLongestPrs().toString());
+        System.out.println(new DatabaseQueryService(date).findYoungOldWorker().toString());
+        System.out.println(new DatabaseQueryService(date).printPrPrice().toString());
         date.close();
     }
 }
